b879921e8a55fed679a1eeac73a7cf40eeaf09eb
danaioa <148085133+danaioa@users.noreply.github.com>
2026-05-31 15:06:31 +0300

    Use relative API url for deploy

--- spendwise-client/src/api/axiosClient.ts
+++ spendwise-client/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5179/api",
+  baseURL: "/api",
 });
 
 axiosClient.interceptors.request.use((config) => {
